00f59b2b6a3ad8b389e134737fe91110329c14ad
Ozgun-Sahin <ozgun_sahin@outlook.com>
2022-09-02 00:55:36 +0300

    first commit

---
+++ Project_1_Phone_Book/Project_1_Phone_Book/Data/Kisi.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_1_Phone_Book.Data
+{
+    public class Kisi
+    {
+        public string Isim { get; set; }
+        public string Soyisim { get; set; }
+        public int Numara { get; set; }
+    }
+}
---
+++ Project_1_Phone_Book/Project_1_Phone_Book/Data/RehberDB.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_1_Phone_Book.Data
+{
+    public class RehberDB
+    {
+        private static List<Kisi> _kisiListesi;
+
+
+        static RehberDB()
+        {
+            _kisiListesi = new List<Kisi>()
+            {
+                new Kisi{Isim = "Özgün", Soyisim ="Şahin", Numara =1234567},
+                new Kisi{Isim = "Damla Naz", Soyisim ="Şahin", Numara =9012345},
+                new Kisi{Isim = "Mehmet", Soyisim ="Şahin", Numara =6789012},
+                new Kisi{Isim = "Esma", Soyisim ="Şahin", Numara =3456789},
+                new Kisi{Isim = "Bilge Kağan", Soyisim ="Şahin", Numara =0123456}
+            };
+        }
+
+        public static List<Kisi> KisiListesi
+        {
+            get { return _kisiListesi; }
+        }
+
+    }
+}
