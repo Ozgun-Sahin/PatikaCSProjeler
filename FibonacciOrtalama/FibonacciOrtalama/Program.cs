13ae1183f066f516fe4e06d1893b276cb63b1b98
Ozgun-Sahin <ozgun_sahin@outlook.com>
2022-09-12 09:35:45 +0300

    fibonacci ortalama

---
+++ FibonacciOrtalama/FibonacciOrtalama/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+
+namespace FibonacciOrtalama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int b = 1;
+            int c = 0;
+            int toplam = 0;
+
+            ArrayList fibo = new ArrayList();
+
+            Console.WriteLine("Kaç elemanlı bir seri olmalı ?");
+            int entry = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < entry; i++)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+
+                toplam = toplam + c;
+
+                fibo.Add(c);
+            }
+
+            Console.WriteLine("serinin elemanları");
+            foreach (var item in fibo)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("serinin ortalaması:");
+
+            int ortlama = toplam / entry + 1;
+
+            Console.WriteLine(ortlama); 
+           
+        }
+    }
+}
