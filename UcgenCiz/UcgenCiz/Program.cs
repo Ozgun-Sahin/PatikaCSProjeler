24911719608983810f9e86d642b9ff549336d08e
Ozgun-Sahin <ozgun_sahin@outlook.com>
2022-09-14 00:40:25 +0300

    UcgenCiz Eklendi

---
+++ UcgenCiz/UcgenCiz/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace UcgenCiz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen bir yükseklik giriniz");
+            int yukseklik = int.Parse(Console.ReadLine());
+
+
+
+            for (int i = 1; i <= yukseklik; i++)
+            {
+                for (int j = 0; j < yukseklik - i; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int k = 0; k < 2 * i - 1; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
