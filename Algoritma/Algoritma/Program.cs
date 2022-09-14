a75c0a366281648766dee4b1a6fad004e32fe4fc
Ozgun-Sahin <ozgun_sahin@outlook.com>
2022-09-14 23:44:24 +0300

    algoritma eklendi

---
+++ Algoritma/Algoritma/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Xml;
+
+namespace Algoritma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen bir kelime giriniz");
+            string kelime = Console.ReadLine();
+
+            int sinir = kelime.Length;
+
+            Console.WriteLine("Lütfen 0 - " + sinir + " arası bir sayı giriniz");
+        SayiGir:
+            try
+            {
+                int sayi = int.Parse(Console.ReadLine());
+
+                if (sayi < sinir)
+                {
+                    Console.WriteLine("input: " + kelime + "," + sayi);
+                    Console.WriteLine("output: " + kelime.Remove(sayi, 1));
+                }
+                else
+                {
+                    Console.WriteLine("Lütfen belirtilen aralıkta bir sayı giriniz");
+                    goto SayiGir;
+                }
+                
+            }
+            catch (Exception )
+            {
+
+                Console.WriteLine("lütfen bir sayı giriniz");
+                goto SayiGir;
+            }
+            
+
+            
+        }
+    }
+}
