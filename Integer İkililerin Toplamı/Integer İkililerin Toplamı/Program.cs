a8593a51a917a4037d227f61eb1212903c3cbd34
Ozgun-Sahin <ozgun_sahin@outlook.com>
2022-09-19 01:00:31 +0300

    int ikililerinin toplamı eklendi

---
+++ Integer İkililerin Toplamı/Integer İkililerin Toplamı/Program.cs
@@ -0,0 +1,47 @@
+﻿Console.WriteLine("Lütfen aralarında boşluk bırakarak sayı giriniz");
+giris:
+try
+{
+    
+    List<int> entry = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+    List<int> yeniList = new List<int>();
+
+    for (int i = 0; i < entry.Count - 1;)
+    {
+        if (entry[i] != entry[i + 1])
+        {
+            int sum = entry[i] + entry[i + 1];
+            yeniList.Add(sum);
+            i = i + 2;
+
+        }
+        else
+        {
+            int sum = (int)Math.Pow((entry[i] + entry[i + 1]), (entry[i] + entry[i + 1]));
+            yeniList.Add((int)sum);
+            i = i + 2;
+        }
+
+    }
+
+    string result = "";
+
+    foreach (var item in yeniList)
+    {
+        result = result + item + " ";
+    }
+
+    Console.WriteLine(result);
+
+}
+catch (Exception)
+{
+
+    Console.WriteLine("Lütfen istenilen formatta giriş yapınız");
+    goto giris;
+}
+
+
+
+
+
