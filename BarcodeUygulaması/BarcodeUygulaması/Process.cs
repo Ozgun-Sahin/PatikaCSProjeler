055e17fdc607f0ad48abfaf83b33659af5451da7
Ozgun-Sahin <ozgun_sahin@outlook.com>
2022-09-26 01:35:02 +0300

    Barcode Uygulaması Eklendi

---
+++ BarcodeUygulaması/BarcodeUygulaması/Process.cs
@@ -0,0 +1,32 @@
+﻿using BarcodeLib;
+using IronBarCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarcodeUygulaması
+{
+    public class Process
+    {
+        static TYPE type = TYPE.PHARMACODE;
+
+        public static Barcode CreateBarcode(string data)
+        {
+            Barcode barcode = new Barcode(data, type);
+
+            barcode.Encode(type, data);
+            barcode.SaveImage(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\barcode.png", BarcodeLib.SaveTypes.PNG);
+            Console.WriteLine("Barcode Generated -> {0}", data);
+            return barcode;
+        }
+
+        public static string ReadBarcode(Barcode barcode)
+        {
+            return "Barcode Value = " + barcode.RawData + "\n";
+        }
+
+
+    }
+}
---
+++ BarcodeUygulaması/BarcodeUygulaması/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BarcodeLib;
+using BarcodeUygulaması;
+
+Barcode barcode = Process.CreateBarcode("66666");
+Console.WriteLine(Process.ReadBarcode(barcode));
